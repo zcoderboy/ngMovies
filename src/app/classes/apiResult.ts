04a69a659f41d1f928e6cf31e168cc57807f2f5a
Samba24 <juniornjay@gmail.com>
2019-05-21 09:44:12 +0000

    Added interface for api requests

---
+++ src/app/classes/apiResult.ts
@@ -0,0 +1,4 @@
+export interface apiResult{
+    page: Number;
+    results: [] ;
+}
--- src/app/services/movie/movie.service.ts
+++ src/app/services/movie/movie.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { apiResult } from 'src/app/classes/apiResult';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +10,8 @@ export class MovieService {
 
   constructor(private http : HttpClient) { }
 
-  getAll(){
+  getAll() : Observable<apiResult>{
     //envoyer les requete http vers API
-    return this.http.get('https://api.themoviedb.org/3/trending/all/day?api_key=c0fe280f740fba250e714fb7aa412a89');
+    return this.http.get<apiResult>('https://api.themoviedb.org/3/trending/all/day?api_key=c0fe280f740fba250e714fb7aa412a89');
   }
 }
