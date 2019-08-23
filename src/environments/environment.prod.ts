4127a1049fe7584fad1ef0b28fd60b67f20319f9
Samba Ndiaye <juniornjay@gmail.com>
2019-08-23 11:23:14 +0000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyD-ep9N0TCSNJW1qmRfJN6cjezqpLEnow8",
+    authDomain: "ngmovies-ab959.firebaseapp.com",
+    databaseURL: "https://ngmovies-ab959.firebaseio.com",
+    projectId: "ngmovies-ab959",
+    storageBucket: "ngmovies-ab959.appspot.com",
+    messagingSenderId: "272200546304",
+    appId: "1:272200546304:web:59d745bc06032211"
+}
 };
